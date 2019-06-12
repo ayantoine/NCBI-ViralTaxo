5f5cdb61ff083a03f4d95167efce0ff5a57d26c9
ayantoine <aymeric.antoine@hotmail.fr>
2019-06-12 12:04:47 +0200

    Fix: dynamic bash file rewrited

--- RewriteListTarget.py
+++ RewriteListTarget.py
@@ -5,13 +5,15 @@
 import sys
 import time
 
-sCurrentVersionScript="v2"
+sCurrentVersionScript="v3"
 iTime1=time.time()
 ########################################################################
 '''
 V2-2019/06/12
-Make code compatible for py2.6 (only version present on Agap)
+Add recovery on error for FlatTax downloading
 
+V2-2019/06/12
+Make code compatible for py2.6 (only version present on Agap)
 V1-2019/06/07
 Create a bash file to download all viral file.seq from genbank
 
@@ -26,8 +28,8 @@ GBK_FTP="ftp://ftp.ncbi.nlm.nih.gov/genbank/"
 #MAIN
 if __name__ == "__main__":
 	FILE=open("DownloadListTarget.sh","w")
-	FILE.write("#! /bin/bash")
-	FILE.write("set -e") #if a command crash, the script interrupt immediatly
+	FILE.write("#! /bin/bash\n")
+	FILE.write("set -e\n") #if a command crash, the script interrupt immediatly
 	for sNewLine in open(sys.argv[1]):
 		sLine=sNewLine.strip()
 		# sModifiedLine="echo \"Downloading {}...\"\ncurl {}{} --output {}\n".format(sLine,GBK_FTP,sLine,sLine)
